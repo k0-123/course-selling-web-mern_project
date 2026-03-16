7f7c2fff86544ae549c2cbff3e74009b86cec1d3
Karan Singh Shekhawat <shekhawatk271@gamil.com>
2026-03-16 23:33:06 +0530

    admin skeletone

---
+++ routes/admin.js
@@ -0,0 +1,36 @@
+const { Router} = require("express");
+const adminRouter = Router();
+ 
+adminRouter.post("/signup" , function(req,res){
+    res.json({
+        message:'signup successfully'
+    })
+})
+
+adminRouter.post("/signin" , function(req,res){
+    res.json({
+        message:'signin successfully'
+    })
+})
+
+adminRouter.post("/courses" , function(req,res){
+    res.json({
+        message:'signin successfully'
+    })
+})
+
+adminRouter.put("/courses" , function(req,res){
+    res.json({
+        message:'signin successfully'
+    })
+})
+
+adminRouter.get("/courses/bulk" , function(req,res){
+    res.json({
+        message:'signin successfully'
+    })
+})
+
+module.exports = {
+    adminRouter : adminRouter
+}
